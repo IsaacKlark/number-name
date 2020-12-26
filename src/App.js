e2dfc1c56f62659b3fa896a96276a114898c84da
IsaacKlark <isaackeklarke@gmail.com>
2020-12-26 19:48:20 +0200

    created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import literes from './litteres';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState("");
+
+
+  const changeName = (nameInput) => {
+    setName(nameInput);
+    let numberInput = 0;
+
+    for (let i = 0; i < nameInput.length; i++) {
+      numberInput += literes.find(value => value.litere === nameInput[i].toLowerCase())?.number || 0;
+    }
+
+    setNumber(numberInput);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <label htmlFor="name" className="name">
+        <p className="nametext">
+          Введите имя:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input
+          type="text"
+          id="name"
+          onChange={e => { changeName(e.target.value) }}
+        />
+      </label>
+
+      <p className="numberName">
+        Число имени: {number}
+      </p>
+      <div className="code">
+        {name.split('').map((value, index, array) => {
+          const numberInput = literes.find(alphabet => alphabet.litere === value.toLowerCase())?.number || 0;
+          if (!numberInput) return;
+          return (<p className="codeNumber" key={Math.random()}>
+              {value} - {numberInput}
+            </p>)
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/litteres.js
@@ -0,0 +1,183 @@
+const literes = [
+    {
+      litere: 'а',
+      number: 1,
+    },
+    {
+      litere: 'б',
+      number: 2,
+    },{
+      litere: 'в',
+      number: 3,
+    },{
+      litere: 'г',
+      number: 4,
+    },{
+      litere: 'д',
+      number: 5,
+    },{
+      litere: 'е',
+      number: 6,
+    },{
+      litere: 'ё',
+      number: 7,
+    },{
+      litere: 'ж',
+      number: 8,
+    },{
+      litere: 'з',
+      number: 9,
+    },{
+      litere: 'и',
+      number: 10,
+    },{
+      litere: 'й',
+      number: 20,
+    },{
+      litere: 'к',
+      number: 30,
+    },{
+      litere: 'л',
+      number: 40,
+    },{
+      litere: 'м',
+      number: 50,
+    },{
+      litere: 'н',
+      number: 60,
+    },{
+      litere: 'о',
+      number: 70,
+    },{
+      litere: 'п',
+      number: 80,
+    },{
+      litere: 'р',
+      number: 90,
+    },{
+      litere: 'с',
+      number: 100,
+    },{
+      litere: 'т',
+      number: 200,
+    },{
+      litere: 'у',
+      number: 300,
+    },{
+      litere: 'ф',
+      number: 400,
+    },{
+      litere: 'х',
+      number: 500,
+    },{
+      litere: 'ц',
+      number: 600,
+    },{
+      litere: 'ч',
+      number: 700,
+    },{
+      litere: 'ш',
+      number: 800,
+    },{
+      litere: 'щ',
+      number: 900,
+    },{
+      litere: 'ъ',
+      number: 1000,
+    },{
+      litere: 'ы',
+      number: 2000,
+    },{
+      litere: 'ь',
+      number: 3000,
+    },{
+      litere: 'э',
+      number: 4000,
+    },{
+      litere: 'ю',
+      number: 5000,
+    },{
+      litere: 'я',
+      number: 6000,
+    },{
+      litere: 'a',
+      number: 1,
+    },{
+      litere: 'b',
+      number: 2,
+    },{
+      litere: 'c',
+      number: 3,
+    },{
+      litere: 'd',
+      number: 4,
+    },{
+      litere: 'e',
+      number: 5,
+    },{
+      litere: 'f',
+      number: 6,
+    },{
+      litere: 'g',
+      number: 7,
+    },{
+      litere: 'h',
+      number: 8,
+    },{
+      litere: 'i',
+      number: 9,
+    },{
+      litere: 'j',
+      number: 10,
+    },{
+      litere: 'k',
+      number: 20,
+    },{
+      litere: 'l',
+      number: 30,
+    },{
+      litere: 'm',
+      number: 40,
+    },{
+      litere: 'n',
+      number: 50,
+    },{
+      litere: 'o',
+      number: 60,
+    },{
+      litere: 'p',
+      number: 70,
+    },{
+      litere: 'q',
+      number: 80,
+    },{
+      litere: 'r',
+      number: 90,
+    },{
+      litere: 's',
+      number: 100,
+    },{
+      litere: 't',
+      number: 200,
+    },{
+      litere: 'u',
+      number: 300,
+    },{
+      litere: 'v',
+      number: 400,
+    },{
+      litere: 'w',
+      number: 500,
+    },{
+      litere: 'x',
+      number: 600,
+    },{
+      litere: 'y',
+      number: 700,
+    },{
+      litere: 'z',
+      number: 800,
+    },
+  ]
+
+  export default literes;
